cc1b66bf9daeece139d21ff536e8f5a593bcf12d
ryan.wang <ryan.wang@aotter.net>
2022-03-24 15:00:28 +0800

    add version endpoint

--- src/main/java/com/ryanwang/test/controller/TestController.java
+++ src/main/java/com/ryanwang/test/controller/TestController.java
@@ -1,13 +1,22 @@
 package com.ryanwang.test.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Value( "${app.version}" )
+    private String appVersion;
+
     @GetMapping("/hello")
     public String hello(){
        return "Hello World";
+    }
+
+    @GetMapping("/version")
+    public String version(){
+        return appVersion ;
     }
 }
